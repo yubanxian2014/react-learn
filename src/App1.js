4d92efaefd724d377e5b9dac7998a7439a503070
xiaofei.yu <xiaofei.yu@micromost.cn>
2019-11-13 10:21:09 +0800

    learn react

---
+++ src/App1.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+export default class App1 extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            count:0
+        }
+    }
+
+    handleIncrement=()=>{
+        this.setState({
+            
+        })
+    }
+}
---
+++ src/model.js
@@ -0,0 +1,5 @@
+let a="a1"
+let b="a2"
+let c="a3"
+export {a,b}
+export default c
